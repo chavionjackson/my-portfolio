38b27d99b1bf9f31110ddb08911d33ff6b2aa6ea
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-07-15 14:57:39 -0700

    Completed functionality, adding styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from "react";
+import logo from "../images/test logo.png";
+import { Link } from "react-scroll";
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav style={{ backgroundColor: "black" }} className="navbar navbar-expand-lg fixed-top">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar
+          <img className="logo" src={logo} alt="logo..." />
         </a>
         <button
           className="navbar-toggler"
@@ -16,19 +21,62 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+          <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
+            <li className="nav-item active">
+              <Link smooth={true} to="home" className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
+              <Link smooth={true} to="about" offset={-110} className="nav-link">
+                about me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                smooth={true}
+                to="services"
+                offset={-110}
+                className="nav-link"
+                href="#"
+              >
+                services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                smooth={true}
+                to="experience"
+                offset={-110}
+                className="nav-link"
+              >
+                experience
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                smooth={true}
+                to="portfolio"
+                offset={-110}
+                className="nav-link"
+              >
+                portfolio
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                smooth={true}
+                to="contacts"
+                offset={-110}
+                className="nav-link"
+              >
+                contacts
+              </Link>
             </li>
           </ul>
         </div>
